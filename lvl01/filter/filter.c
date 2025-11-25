32554d17fb86818f28ac88c11ad9ce0bb2ed31d7
Aurelio Floris <babyf@MacBook-Air-di-Auri.local>
2025-11-25 17:42:50 +0100

    filter.c

--- lvl01/filter/filter.c
+++ lvl01/filter/filter.c
@@ -6,7 +6,7 @@
 /*   By: babyf <babyf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 16:38:42 by babyf             #+#    #+#             */
-/*   Updated: 2025/11/23 12:15:55 by babyf            ###   ########.fr       */
+/*   Updated: 2025/11/25 17:42:35 by babyf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,31 @@
 
 # define BUFFER_SIZE 10000 + 1
 
+void	ft_filter(char *buffer, const char *target)
+{
+	/* counters and a strlen for target */
+	int	i, j, k;
+	int	target_len;
+
+	i = 0;
+	target_len = strlen(target);
+	/* start a while loop */
+	while (buffer[i])
+	{
+		/* initialize second counter */
+		/* check for any occurence of arg s [while loop]*/
+		/* init third counter */
+		/* once the occurance is found [if]
+			write * for occurence of arg s 
+			increment 
+		skip arg s */
+		/* if occurance is not found [else]
+			just write arg s
+			increment */
+	}
+}
+
+int	main(int ac, char **av)
+{
+	
+}
