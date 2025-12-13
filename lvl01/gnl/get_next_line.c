7435b34e219db80e791bea7e5331468cf278695f
Aurelio Floris <babyf@Mac.station>
2025-12-13 10:58:02 +0100

    sooooo depressed

--- lvl01/gnl/get_next_line.c
+++ lvl01/gnl/get_next_line.c
@@ -1,12 +1,83 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   easy_gnl.c                                         :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: babyf <babyf@student.42.fr>                +#+  +:+       +#+        */
+/*   By: afloris <afloris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 12:21:19 by babyf             #+#    #+#             */
-/*   Updated: 2025/12/05 17:50:14 by babyf            ###   ########.fr       */
+/*   Updated: 2025/12/13 10:57:35 by afloris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strdup(char *str)
+{
+	int		i;
+	int		j;
+	char	*new;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+		i++;
+	new = (char *)malloc(i + 1);
+	if (!new)
+		return (NULL);
+	while (str[j])
+	{
+		new[j] = str[j];
+		j++;
+	}
+	new[j] = '\0';
+	return (new);
+}
+
+char	*get_next_line(int fd)
+{
+	int			i;
+	static char	buff[BUFFER_SIZE];
+	static int	buff_read;
+	static int	buff_pos;
+	char		line[70000];
+
+	i = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (1)
+	{
+		if (buff_pos >= buff_read)
+		{
+			buff_read = read(fd, buff, BUFFER_SIZE);
+			buff_pos = 0;
+			if (buff_read <= 0)
+				break;
+		}
+		line[i++] = buff[buff_pos++];
+		if (line[i - 1] == '\n')
+			break ;
+	}
+	line[i] = '\0';
+	if (i == 0)
+		return (NULL);
+	return (ft_strdup(line));
+}
+
+/*
+int	main()
+{
+	int		fd;
+	char	*line;
+
+	fd = open("test", O_RDONLY);
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("%s", line);
+		free (line);
+		line = get_next_line(fd);
+	}
+}
+*/
 
--- lvl01/gnl/get_next_line.h
+++ lvl01/gnl/get_next_line.h
@@ -3,10 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: babyf <babyf@student.42.fr>                +#+  +:+       +#+        */
+/*   By: afloris <afloris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 17:50:45 by babyf             #+#    #+#             */
-/*   Updated: 2025/12/05 17:50:49 by babyf            ###   ########.fr       */
+/*   Updated: 2025/12/13 10:56:39 by afloris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#define BUFFER_SIZE 42
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#endif 
