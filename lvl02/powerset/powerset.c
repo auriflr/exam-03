37f01e50fddd898189a2ea33e29ef4794b281783
Aurelio Floris <aureliofloris6@gmail.com>
2026-01-14 18:01:15 +0100

    powerset

---
+++ lvl02/powerset/powerset.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   powerset.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: babyf <babyf@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/14 17:46:35 by babyf             #+#    #+#             */
+/*   Updated: 2026/01/14 18:00:15 by babyf            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void	ft_powerset(int set[])
+{
+	
+}
+
+void	ft_solution(int n, int set[])
+/* args: int n (num), int subset[]*/
+
+int	main(int ac, char **av)
+{
+	if (ac < 3)
+		return (0);
+}
