fd88491635ad0438f9af8b9e888b261d3cd319da
Aurelio Floris <aureliofloris6@gmail.com>
2026-01-15 17:48:00 +0100

    missing rip and tsp

--- lvl02/tsp/tsp.c
+++ lvl02/tsp/tsp.c
@@ -1,30 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rip.c                                              :+:      :+:    :+:   */
+/*   tsp.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: babyf <babyf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/15 16:46:53 by babyf             #+#    #+#             */
-/*   Updated: 2026/01/15 17:35:05 by babyf            ###   ########.fr       */
+/*   Created: 2026/01/15 17:43:40 by babyf             #+#    #+#             */
+/*   Updated: 2026/01/15 17:46:17 by babyf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <unistd.h>
-
-/* parse the input */
-void	ft_parse(int *set)
-{
-
-}
-
-void	print_solution(int *set)
-{
-
-}
-
-int	main(int ac, char **av)
-{
-	
-}
+#include <stdio.h>
+#include <stdlib.h>
