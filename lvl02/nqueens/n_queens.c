15ede6b6ed8ffde9d3b351d36883cf6b0b13bed2
Aurelio Floris <aureliofloris6@gmail.com>
2026-02-16 16:36:13 +0100

    new

--- lvl02/nqueens/n_queens.c
+++ lvl02/nqueens/n_queens.c
@@ -6,26 +6,26 @@
 /*   By: babyf <babyf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 15:44:41 by babyf             #+#    #+#             */
-/*   Updated: 2026/02/11 17:17:30 by babyf            ###   ########.fr       */
+/*   Updated: 2026/02/13 11:34:43 by babyf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "n_queens.h"b
+#include "n_queens.h"
 
+/* fix this function */
 void	put_array(int board[], int n)
 {
 	int		i;
-	char	c;
+
 	i = 0;
 	while (i < n)
 	{
-		c = board[i] + '0';
-		write (1, &c, 1);
+		fprintf(stdout, "%d", board[i]);
 		if (i < n - 1)
-			write (1, " ", 1);
+			fprintf(stdout, " ");
 		i++;
 	}
-	write (1, "\n", 1);s
+	fprintf(stdout, "\n");
 }
 
 int	is_safe(int board[], int row, int col)
